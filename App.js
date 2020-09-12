60f1e4fc79c303d3207e01c3b2aa6d029ebd8693
Gizele Santos <gizele.santos@indraenergia.com>
2020-09-12 11:37:31 -0300

    Criando projeto usando api openweathermap para retornar infos especificas

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  TextInput, 
+  View, 
+  FlatList, 
+  Button, 
+  Keyboard } 
+  from 'react-native';
+
+import PrevisaoItem from './components/PrevisaoItem';
 
 export default function App() {
+
+  const [cidade, setCidade] = useState ('');
+  const [previsoes, setPrevisoes] = useState ([]);
+  
+  const capturarCidade = (cidade) => {
+    setCidade(cidade);
+  }
+
+  const obterPrevisoes = () => {
+    setPrevisoes([]);
+    const target = endPoint + cidade + "&appid=" + apiKey;
+    fetch(target)
+    .then((dados) => {
+      return dados.json()
+    })
+    .then((dados) => {
+      setPrevisoes (dados["list"])
+      Keyboard.dismiss()
+    });
+    
+  };
+
+  const endPoint = "https://api.openweathermap.org/data/2.5/forecast?lang=pt_br&units=metric&q=";
+  const apiKey = '7dcab7984820ba169205e706fecd4ce1';
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.entrada}>
+        <TextInput 
+          style={styles.nomeCidade}
+          placeholder="Digite o nome da cidade"
+          value={cidade}
+          onChangeText={capturarCidade}
+        />
+        <Button 
+          title="ok"
+          onPress={obterPrevisoes}
+        />
+      </View>
+      <FlatList
+        data={previsoes}
+        renderItem={
+          previsao =>(
+            <PrevisaoItem previsao={previsao.item} />
+          )
+        } 
+      />
+     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 40,
+    flexDirection: 'column',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFF'
   },
+  entrada: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8 
+  },
+  nomeCidade: {
+    padding: 12,
+    borderBottomColor: '#BB96F3',
+    borderBottomWidth: 2,
+    textAlign: 'left',
+    flexGrow: 0.9
+  }
 });
